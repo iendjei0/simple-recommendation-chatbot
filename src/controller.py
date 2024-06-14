5cccb54c31408196913db5bb12310320d249ad64
iendjei <iendjei@o2.pl>
2024-06-14 22:02:05 +0200

    Create a barebone GUI

--- src/controller.py
+++ src/controller.py
@@ -6,9 +6,4 @@ class ChatController():
         self._model = ChatModel()
         self._view = ChatView()
     def run(self) -> None:
-        while True:
-            prompt = input("Prompt: ")
-            if prompt in ["exit", "quit", "q"]:
-                break
-            response = self._model.get_response(prompt)
-            self._view.show_text(response)
+        self._view.mainloop()
--- src/view.py
+++ src/view.py
@@ -1,6 +1,37 @@
-class ChatView():
+import customtkinter as ctk
+
+ctk.set_appearance_mode("dark")
+
+WINDDOW_WIDTH = 650
+WINDDOW_HEIGHT = 600
+X_PADDING = 30
+Y_PADDING = 10
+CORNER_RADIUS = 15
+
+class ChatView(ctk.CTk):
     def __init__(self):
-        pass
+        super().__init__() 
+        FONT = ctk.CTkFont(family="Arial", size=14)
+
+        self.title("MyChat")
+        self.geometry(f"{WINDDOW_WIDTH}x{WINDDOW_HEIGHT}")
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure(0, weight=1)
+
+        self.chat_box = ctk.CTkTextbox(self, corner_radius=CORNER_RADIUS, font=FONT)
+        self.chat_box.grid(column=0, row=0, padx=X_PADDING, pady=Y_PADDING, sticky="news", columnspan=2)
+
+
+        self.user_part = ctk.CTkFrame(self, corner_radius=CORNER_RADIUS)
+        self.user_part.grid(column=0, row=1, padx=X_PADDING, pady=Y_PADDING, sticky="ew")
+        self.user_part.columnconfigure(0, weight=1)
+
+        self.user_box = ctk.CTkTextbox(self.user_part, corner_radius=CORNER_RADIUS, font=FONT)
+        self.user_box.grid(column=0, row=0, sticky="ew")
+        self.send_button = ctk.CTkButton(self.user_part, text="Send", corner_radius=CORNER_RADIUS, font=FONT)
+        self.send_button.grid(column=1, row=0, sticky="", padx=20)
+
+
 
     def show_text(self, text: str) -> None:
         print(text)

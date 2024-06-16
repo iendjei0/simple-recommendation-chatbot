294bbe265041a90b6154a5bb719a13c227f9bf0f
iendjei <iendjei@o2.pl>
2024-06-16 18:16:38 +0200

    Make model train only when it needs to

--- src/model.py
+++ src/model.py
@@ -1,11 +1,16 @@
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
+from chatterbot.storage import SQLStorageAdapter
 
 class ChatModel(ChatBot):
     def __init__(self):
         super().__init__('Greeter')
-        self._trainer = ChatterBotCorpusTrainer(self)
-        self._trainer.train("chatterbot.corpus.english.greetings")
+        if not SQLStorageAdapter().count():
+            trainer = ChatterBotCorpusTrainer(self)
+            trainer.train("chatterbot.corpus.english.food")
+            trainer.train("chatterbot.corpus.english.greetings")
+            trainer.train("chatterbot.corpus.english.conversations")
+            trainer.train("chatterbot.corpus.english.emotion")
 
-    def get_response(self, prompt: str) -> str:
+    def get_casual_response(self, prompt: str) -> str:
         return super().get_response(prompt)
